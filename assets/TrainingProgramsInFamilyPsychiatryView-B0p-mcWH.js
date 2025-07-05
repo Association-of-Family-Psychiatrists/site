aea42fe9fc81f81b305ebbb9347739128d8e8b6c
Tanuj KS <tanujsiripurapu@gmail.com>
2025-07-05 16:33:53 -0700

    Updates

---
+++ assets/TrainingProgramsInFamilyPsychiatryView-B0p-mcWH.js
@@ -0,0 +1,73 @@
+import{C as i}from"./ContentPage-Dc7t9rt7.js";import{c as a,o as t}from"./index-DgQ9eTsl.js";const c={__name:"TrainingProgramsInFamilyPsychiatryView",setup(r){const e=[{heading:"Overview",subheading:"Family systems training opportunities in psychiatric residency programs",paragraphs:[`
+Today, with the rise in the popularity of psychopharmacology and the promise of biological interventions, there are fewer opportunities for family systems training within psychiatric residency programs. In order to receive family systems training, a psychiatrist may decide to enroll in an independent family training institute, such as the [Ackerman Institute for the Family](http://www.ackerman.org/) in New York. However, there are still some psychiatric residency programs that consider learning to work with families to be an essential psychotherapeutic skill.
+      `]},{heading:"Psychiatric Training in Family Therapy",subheading:"Programs offering family systems training in psychiatric residency",paragraphs:[`
+Dr. Ellen Berman, president of the Association of Family Psychiatrists, recently asked family psychiatrists to indicate psychiatric residency programs offering family systems training. Here is a description of each program and contact information:
+
+**Albert Einstein College of Medicine, New York**
+"We have a well-established and extensive Family Studies program that is part of the department of psychiatry at Albert Einstein College of Medicine and is well integrated into the residency training program. All residents in the 4 years of the training program at Montefiore Medical Center and Bronx (N.Y.) Psychiatric Center as well as child fellows receive supervision in couples and family therapy, courses, seminars, electives, and more. Faculty and residents have presented different innovative projects related to family and systems at national conferences and have written papers on their clinical work."
+
+*Contact:* Madeleine.Abrams@omh.ny.gov, Director of Family Studies, department of psychiatry, Albert Einstein College of Medicine.
+
+**McGill University, Montreal**
+"We have a very active family therapy community in Montreal, with [Dr.] Herta Guttman as our pioneer. At the Jewish General [Hospital] department of psychiatry, we run an [American Association for Marriage and Family Therapy](http://www.aamft.org/iMIS15/AAMFT/)–accredited postgraduate certificate couple and [family therapy program](http://www.jgh.ca/en/CoupleandFamilyTherapyTrainingProgram). This program primarily trains community mental health practitioners (social workers, psychologists, etc.), however, we have had psychiatrists take this course. This year, we are training our first [Couple and Family Fellow](https://www.medicine.mcgill.ca/postgrad/docs/Fellowships/Couple%20and%20Family%20Therapy%20Fellowship.pdf) (child psychiatrist) through the department of psychiatry.
+
+In McGill Psychiatry, we have a new family skills teaching module in the McGill Resident Diploma Program. We will provide 3 hours of teaching in the R1 year (basic concepts). In the R2 and R3 years, residents receive training on a range of family therapy topics. The curriculum was inspired by the[ Group for the Advancement of Psychiatry](http://ourgap.org/) curriculum. Dr. Nick Casacalenda spearheaded this initiative. There are 3 hours of family teaching in the diploma course module: gender, social, and cultural aspects of psychiatry, and 6 hours in the child psychiatry module."
+
+*Contact:* [Sharon Bond, Ph.D.](sharon.bond@mcgill.ca); School of Social Work; Director, Couple and Family Therapy Program.
+
+**NYU School of Medicine**
+"I am the director of service at the Roberto Clemente Center in the Lower East Side (now East Village) in Manhattan. The center is part of the departments of mental health of both [Gouverneur Healthcare Services](http://www.nyc.gov/html/hhc/gouverneur/html/about/centers.shtml) and [Bellevue](http://www.nyc.gov/html/hhc/bellevue/html/home/home.shtml). We have an academic affiliation with New York University School of Medicine. The center was started about 30 years ago by a psychologist, Jaime Inclan, Ph.D. He was initially was trained by [Dr. Salvador Minuchin](http://www.psychotherapy.net/data/uploads/5113e45715ce5.pdf). As a family clinic embedded in the community, we mostly serve minority underserved populations. We provide primary health and mental health services. We have been providing for decades training to medical students, psychology interns, and social work students."
+
+*Contact:* Dr. Miguel Vilaro-Colon, Clinical Assistant Professor of Psychiatry, NYU School of Medicine.
+
+**Stanford University School of Medicine**
+"Our 4-year training program in couples and family therapy begins with didactics and family meetings (inpatient) in the first year; didactics and opportunities for a couples and family therapy clinical elective as [postgraduate year]-2s; live case observation in the family therapy program at the [Veterans Affairs] Palo Alto Health Care System, as well as an intensive seminar at Stanford during the PGY-3 year, and supervised outpatient clinical work in Stanford's Couples and Family Therapy Clinic during the PGY-3 and PGY-4 years. Residents who are interested in child and adolescent psychiatry can focus their PGY-3 rotation on family cases with a child/adolescent focus. An earlier version of this training sequence is described in 'A Model for Reintegrating Couples and Family Therapy Training in Psychiatric Residency Programs' " (Acad. Psychiatry 2008;32:81-6).
+
+*Contact:* Douglas S. Rait, Ph.D., Chief, Couples and Family Therapy Clinic, Clinical Professor of Psychiatry & Behavioral Sciences, Stanford University.
+
+**Mount Sinai Beth Israel, New York**
+The Family Center for Bipolar provides services to patients with bipolar disorder "within the context of the family." In addition, Dr. Igor Galynker, who runs the center, gives a series of lectures on family inclusion. The center also has a formal training program in family therapy run by Haya Mermelstein, CSW.
+
+*Contact:* Dr. Igor Galynker, professor of psychiatry, Icahn School of Medicine at Mount Sinai; director, the Family Center for Bipolar Disorder; Associate Chairman, Department Of Psychiatry and Behavioral Sciences.
+
+**Brown University, Providence, R.I.**
+"We have a formal Family Therapy Training Program in the department of psychiatry at Brown University, based at Rhode Island Hospital."
+
+*Contact:* Dr. Gabor I. Keitner, Associate Chief of Psychiatry, Rhode Island and Miriam Hospitals; Professor, Department of Psychiatry, Brown University.
+
+**University of Rochester, Institute for the Family**
+"An emphasis on family is embedded in all of our training. The residents have a formal didactic curriculum and participate in family care and family research. Many of the residents fast track into a child fellowship, which has an extensive program in family therapy."
+
+*Contact:* Dr. Glenn_Currier@URMC.rochester.edu, Director of Psychiatry Residency Education, University of Rochester Medical Center.
+
+**University of Colorado Denver**
+"Our department offers a unique psychotherapy scholar track. The mission of this track is to offer advanced psychotherapy training to a subgroup of psychiatric residents, within an adult general residency program, who wish to learn and do psychotherapy. Within this track we offer extensive family therapy training. The family therapy component consists of didactics and direct supervision. It runs through the 4 years of residency. There are several experienced family therapists on the faculty who supervise in this program."
+
+*Contact:* Dr. Robert.Feinstein@ucdenver.edu, Vice Chair, Department of Psychiatry.
+      `]},{heading:"A Multidisciplinary Field",subheading:"Family systems training across different disciplines",paragraphs:[`
+Family systems training occurs in other disciplines: psychology, social work, and marriage and family therapy (MFT) programs. The number of MFT programs across the United States is large, reflecting the demand for family systems therapists.
+
+**Psychology and Family Therapy**
+Psychologists with a special interest in family therapy belong to the American Psychological Association's Division 43. Division 43 supports family-oriented clinical and scientific activities as well as education and public policy, a journal called the Journal of Family Psychology, and a quarterly newsletter called The Family Psychologist. Many psychologists are trained in family therapy work in primary care settings. The current president of American Psychological Association, Nadine J. Kaslow, Ph.D., is a family therapist who helps underserved and underprivileged populations receive culturally competent, evidence-based, biopsychosocially oriented mental health services.
+
+**Social Work and Family Therapy**
+Social Workers are required to take two exams: one to be "licensed" as an LCSW (licensed clinical social worker) after graduation with a master's degree. After 3,000 hours of supervised clinical practice, social worker takes second exam for independent licensure LICSW (licensed independent clinical social worker).
+
+Social work schools have a course or two in family therapy. After graduation, students may choose to specialize in couples and family therapy. Like psychiatric and psychological training programs, there is wide variation in the amount of family therapy taught in schools of social work.
+
+**Marriage and Family Therapy (MFT)**
+Marriage and family therapists (MFTs) have their own organization, the American Association for Marriage and Family Therapy (AAMFT). This organization's training is specific to families and couples. Members are required to be supervised in clinical practice for 2 years. AAMFT produces the Journal of Marital and Family Therapy. The AAMFT website also lists accredited programs in the United States and Canada.
+
+**Interdisciplinary Journals and Organizations**
+Two American journals in this area are interdisciplinary: Family Process and Families, Systems & Health. Family Process has broad representation on its board from all disciplines and has a strong focus on family systems research and social justice. Family Process aims to support emerging researchers and clinicians worldwide, and periodically offers grants. Families, Systems & Health is multidisciplinary with a focus on research and clinical practice in medical illness, and health psychology.
+
+The American Family Therapy Academy (AFTA) is the only organization that is interdisciplinary. Founded in 1977, AFTA's objectives include "the advancement of theories, therapies, research, and professional education that regard the family as a unit in a social context, to make information about family therapy available to practitioners in other fields of knowledge and to the public and to foster collaboration among the medical, psychological, social, legal, and other professions that serve families and the science and practice of family therapy."
+
+AFTA is outspoken on issues that affect families. For example, its Immigration Position Statement addresses the negative impact of U.S. immigration policy on families and children. AFTA has strong views about the DSM-5, stating that "the current revision of the DSM continues a long history of ignoring research and excluding vital contributions of nonpsychiatric mental health disciplines resulting in invalid diagnostic categories and treatment protocols. The DSM is dominant in determining mental health diagnosis and treatment and is more harmful than helpful in delineating best practices." The next AFTA conference is in June 2015, and the theme is "Global Ecologies: Connections among Self, Families, Communities, and Cultures."
+      `]},{heading:"Conclusion",subheading:"Accessing family systems training for psychiatrists",paragraphs:[`
+Family therapy has grown from a small group of interested academics, mostly psychiatrists, to a large group of interdisciplinary professionals. Today, psychiatrists have less access to family systems training than in previous decades but can still access training. In addition to attending conferences, psychiatrists interested in this area might see whether training programs would accept them into their courses.
+
+For those psychiatrists who have not had formal training during residency training, this review of programs is a good starting point for seeking family systems training.
+
+*Dr. Heru is with the department of psychiatry at the University of Colorado Denver, Aurora. She is editor of the recently published book Working With Families in Medical Settings: A Multidisciplinary Guide for Psychiatrists and Other Health Professionals (New York: Routledge, 2013).*
+      `]}];return(n,s)=>(t(),a(i,{title:"Training Programs in Family Psychiatry",sections:e}))}};export{c as default};
